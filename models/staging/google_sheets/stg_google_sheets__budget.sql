910ad76c7e5335c547bc2c15a9a91e52b2edeb1d
Jose Del Río <joseantoniodelriocaba@gmail.com>
2024-11-20 12:48:32 +0000

    Transformación src_budget

--- models/staging/google_sheets/stg_google_sheets__budget.sql
+++ models/staging/google_sheets/stg_google_sheets__budget.sql
@@ -8,16 +8,17 @@
 WITH src_budget AS (
     SELECT * 
     FROM {{ source('google_sheets', 'budget') }}
-    ),
+),
 
 renamed_casted AS (
     SELECT
           _row
         , product_id
         , quantity
-        , month
+        , month AS fecha_pedido_productos
+        , TO_CHAR(month, 'Mon') AS month_of_year
         , _fivetran_synced AS date_load
     FROM src_budget
-    )
+)
 
 SELECT * FROM renamed_casted
